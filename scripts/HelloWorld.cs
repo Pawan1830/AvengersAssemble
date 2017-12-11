3db84716e0506f5eaf896ca68c3fa93c4b1bf8f5
Ranjeet Kumar <34417445+rkranjeet@users.noreply.github.com>
2017-12-11 21:36:37 +0530

    Helloworld(C#)

---
+++ scripts/HelloWorld.cs
@@ -0,0 +1,16 @@
+// LANGUAGE: C#
+// AUTHOR: Ranjeet Kumar
+// GITHUB: https://github.com/rkranjeet
+// TUTORIAL LINK: [Tut for C#](https://www.codeproject.com)
+
+using System;
+
+public class HelloWorld
+{ 
+  public static void Main()
+  {
+    Console.WriteLine("HELLO MNNII!");
+  }
+} 
+
+3
